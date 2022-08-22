e9703afa173808c77775422b00ec69ba266c8d1b
Henrique <Henriferreira99@gmail.com>
2022-08-22 12:17:08 -0300

    hotfixes finais testes

--- animals/tests/test_models.py
+++ animals/tests/test_models.py
@@ -3,61 +3,59 @@ from animals.models import Animal
 from groups.models import Group
 from traits.models import Trait
 
-class AnimalModelsTests(TestCase):
+class AnimalModelsTests(TestCase): 
+    def setUpTestData(cls):     
+        cls.animal = {"name": "Carlos", "age": 6, "weight": 23, "sex": "Macho"}
+        cls.group = {"name": "Gato", "scientific_name": "cats familiaris"}
+        cls.trait1_data = {"name": "peludo"}
+        cls.trait2_data = {"name": "médio porte"}
 
-    ... 
-    # def setUpTestData(cls):     
-    #     cls.animal = {"name": "Carlos", "age": 6, "weight": 23, "sex": "Macho"}
-    #     cls.group = {"name": "Gato", "scientific_name": "cats familiaris"}
-    #     cls.trait1_data = {"name": "peludo"}
-    #     cls.trait2_data = {"name": "médio porte"}
+        cls.t1 = Trait.objects.create(**cls.trait1_data)
+        cls.t2 = Trait.objects.create(**cls.trait2_data)
+        cls.group1 = Group.objects.create(**cls.group)
+        cls.animals = Animal.objects.create(**cls.animal, group = cls.group1)
+        cls.animals.traits.add(cls.t1)
+        cls.animals.traits.add(cls.t2)
 
-    #     cls.t1 = Trait.objects.create(**cls.trait1_data)
-    #     cls.t2 = Trait.objects.create(**cls.trait2_data)
-    #     cls.group1 = Group.objects.create(**cls.group)
-    #     cls.animals = Animal.objects.create(**cls.animal, group = cls.group1)
-    #     cls.animals.traits.add(cls.t1)
-    #     cls.animals.traits.add(cls.t2)
-
-    # def test_name_field(self):
-    #     self.assertEqual(self.animals.name, self.animal["name"])
+    def test_name_field(self):
+        self.assertEqual(self.animals.name, self.animal["name"])
     
-    # def test_sex_field(self):
-    #     self.assertEqual(self.animals.sex, self.animal["sex"])
+    def test_sex_field(self):
+        self.assertEqual(self.animals.sex, self.animal["sex"])
 
-    # def tests_weight_field(self):
-    #     self.assertEqual(self.animals.weight, self.animal["weight"])
+    def tests_weight_field(self):
+        self.assertEqual(self.animals.weight, self.animal["weight"])
 
-    # def test_age_field(self):
-    #     self.assertEqual(self.animals.age, self.animal["age"])
+    def test_age_field(self):
+        self.assertEqual(self.animals.age, self.animal["age"])
 
-    # def test_name_max_length(self):
-    #     max_length = self.animals._meta.get_field('name').max_length
-    #     self.assertEquals(max_length, 50)
+    def test_name_max_length(self):
+        max_length = self.animals._meta.get_field('name').max_length
+        self.assertEquals(max_length, 50)
     
-    # def test_group_name_max_length(self):
-    #     max_length = self.group1._meta.get_field('name').max_length
-    #     self.assertEquals(max_length, 20)
+    def test_group_name_max_length(self):
+        max_length = self.group1._meta.get_field('name').max_length
+        self.assertEquals(max_length, 20)
 
-    # def test_group_scientific_name_max_length(self):
-    #     max_length = self.group1._meta.get_field('scientific_name').max_length
-    #     self.assertEquals(max_length, 50)    
+    def test_group_scientific_name_max_length(self):
+        max_length = self.group1._meta.get_field('scientific_name').max_length
+        self.assertEquals(max_length, 50)    
 
-    # def test_trait_name_max_length(self):
-    #     max_length = self.t1._meta.get_field('name').max_length
-    #     self.assertEquals(max_length, 20)
-    #     max_length = self.t2._meta.get_field('name').max_length
-    #     self.assertEquals(max_length, 20)
+    def test_trait_name_max_length(self):
+        max_length = self.t1._meta.get_field('name').max_length
+        self.assertEquals(max_length, 20)
+        max_length = self.t2._meta.get_field('name').max_length
+        self.assertEquals(max_length, 20)
 
-    # def test_sex_max_length(self):
-    #     max_length = self.animals._meta.get_field('sex').max_length
-    #     self.assertEquals(max_length, 15)
+    def test_sex_max_length(self):
+        max_length = self.animals._meta.get_field('sex').max_length
+        self.assertEquals(max_length, 15)
     
-    # def test_group_name(self):
-    #     self.assertEquals(self.group1.name, self.group["name"])
+    def test_group_name(self):
+        self.assertEquals(self.group1.name, self.group["name"])
     
 
-class Relations_animals(TestCase):
+class Relations_group(TestCase):
 
     @classmethod
     
@@ -91,4 +89,31 @@ class Relations_animals(TestCase):
         for animal in self.animalsgroups:
             self.assertNotIn(animal, self.groupo1.group.all())
             self.assertIs(animal.group, self.groupo2)
+
+class Relations_traits(TestCase):
+    @classmethod
+    
+    def setUpTestData(cls):
+
+        cls.animal = {"name": "Carlos", "age": 6, "weight": 23, "sex": "Macho"}
+        cls.gropo1 = {"name": "Gato", "scientific_name": "Gato familiaris"}
+        cls.trait = {"name": "peludo"}
+
+        cls.t1 = Trait.objects.create(**cls.trait)
+        cls.groupoo1 = Group.objects.create(**cls.gropo1)
+        cls.animalstraits = [
+            Animal.objects.create(**cls.animal, group = cls.groupoo1)
+
+            for _ in range(20)
+                
+        ]
+
+    def test_many_traits_in_animals(self):
+       
+        for animal in self.animalstraits:
+            self.t1.trait.add(animal)
+        
+        self.assertEqual(len(self.animalstraits), self.t1.trait.count())
+        for animal in self.animalstraits:
+            self.assertIn(self.t1, animal.traits.all())
             
